d722afd46db32b190d285337b1c18849d4b4e9e9
Горбатков Андрей Александрович (4055142) <agorbatkov@vtb.ru>
2022-12-03 15:57:53 +0300

    Убрал статические методы Убрал дублирование кода при поиске аннотируемых методов Обработка ошибок, при вызове методов через рефлексию

--- hw06-annotations/src/main/java/ru/otus/Runner.java
+++ hw06-annotations/src/main/java/ru/otus/Runner.java
@@ -9,74 +9,65 @@ import ru.otus.test.TestClass;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
+    private static final List<Method> testMethods = new ArrayList<>();
+    private static final List<Method> beforeMethods = new ArrayList<>();
+    private static final List<Method> afterMethods = new ArrayList<>();
+    private static final Statistics statistics = new Statistics();
+
     public static void main(String[] args) {
-        runTest(TestClass.class);
-        Statistics.printStatistics();
+        Runner runner = new Runner();
+        runner.runTest(TestClass.class);
+        statistics.printStatistics();
     }
 
-    private static void runTest(Class<TestClass> clazz) {
-
+    private void runTest(Class<TestClass> clazz) {
+        getAnnotatedMethods(clazz);
         for (Method method :
-                getAnnotatedTestMethods(clazz)) {
+                testMethods) {
             TestClass instance = ReflectionHelper.instantiate(clazz);
-            List<Method> annotatedBeforeMethods = getAnnotatedBeforeMethods(clazz);
-            List<Method> annotatedAfterMethods = getAnnotatedAfterMethods(clazz);
 
-            if (!annotatedBeforeMethods.isEmpty()) {
-                annotatedBeforeMethods.forEach(m -> ReflectionHelper.callMethod(instance, m.getName()));
+            if (!beforeMethods.isEmpty()) {
+                try {
+                    beforeMethods.forEach(m -> ReflectionHelper.callMethod(instance, m.getName()));
+                } catch (Throwable t) {
+                    throw new RuntimeException("Ошибка при выполнении метода с аннотацией @Before");
+                }
             }
             try {
-                Statistics.increasePassedTest();
+                statistics.increasePassedTest();
                 String methodName = method.getName();
                 System.out.printf("Выполняется тест: %s\n", methodName);
                 ReflectionHelper.callMethod(instance, methodName);
-                Statistics.increaseSuccessfulTest();
+                statistics.increaseSuccessfulTest();
                 System.out.println("Тест пройден успешно");
             } catch (Throwable t) {
-                Statistics.increaseFailedTest();
+                statistics.increaseFailedTest();
                 System.out.println("Тест упал с ошибкой");
                 t.printStackTrace();
             }
-            if (!annotatedAfterMethods.isEmpty()) {
-                annotatedAfterMethods.forEach(m -> ReflectionHelper.callMethod(instance, m.getName()));
+            if (!afterMethods.isEmpty()) {
+                try {
+                    afterMethods.forEach(m -> ReflectionHelper.callMethod(instance, m.getName()));
+                } catch (Throwable t) {
+                    throw new RuntimeException("Ошибка при выполнении метода с аннотацией @After");
+                }
             }
         }
     }
 
-    private static List<Method> getAnnotatedTestMethods(Class<TestClass> clazz) {
-        List<Method> testMethods = new ArrayList<>();
+    private void getAnnotatedMethods(Class<TestClass> clazz) {
         for (Method method :
                 clazz.getDeclaredMethods()) {
             if (method.isAnnotationPresent(Test.class)) {
                 testMethods.add(method);
+            } else if (method.isAnnotationPresent(Before.class)) {
+                beforeMethods.add(method);
+            } else if (method.isAnnotationPresent(After.class)) {
+                afterMethods.add(method);
             }
         }
-        return testMethods;
-    }
-
-    private static List<Method> getAnnotatedBeforeMethods(Class<TestClass> clazz) {
-        List<Method> methods = new ArrayList<>();
-        for (Method method :
-                clazz.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(Before.class)) {
-                methods.add(method);
-            }
-        }
-        return methods;
-    }
-
-    private static List<Method> getAnnotatedAfterMethods(Class<TestClass> clazz) {
-        List<Method> methods = new ArrayList<>();
-        for (Method method :
-                clazz.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(After.class)) {
-                methods.add(method);
-            }
-        }
-        return methods;
     }
 }
--- hw06-annotations/src/main/java/ru/otus/statistics/Statistics.java
+++ hw06-annotations/src/main/java/ru/otus/statistics/Statistics.java
@@ -5,19 +5,19 @@ public class Statistics{
     private static int numberSuccessfulTests;
     private static int numberFailedTests;
 
-    public static void increasePassedTest() {
+    public void increasePassedTest() {
         numberPassedTests++;
     }
 
-    public static void increaseSuccessfulTest() {
+    public void increaseSuccessfulTest() {
         numberSuccessfulTests++;
     }
 
-    public static void increaseFailedTest() {
+    public void increaseFailedTest() {
         numberFailedTests++;
     }
 
-    public static void printStatistics() {
+    public void printStatistics() {
         System.out.printf("Всего тестов пройдено: %d\n", numberPassedTests);
         System.out.printf("Тестов пройдено успешно: %d\n", numberSuccessfulTests);
         System.out.printf("Тестов упало с ошибкой: %d\n", numberFailedTests);
